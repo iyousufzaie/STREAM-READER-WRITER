347441d15764ecde79bfc16cc5fe6d2b3651f475
iyousufzaie <41523516+iyousufzaie@users.noreply.github.com>
2018-07-22 03:12:03 +0500

    STREAM READER WRITER

---
+++ StreamReader_Writer/StreamReader_Writer/Form3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace StreamReader_Writer
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            this.Text = "Stream writer";
+            this.label1.Text = "File location";
+            this.label2.Text = "File Name";
+            this.button1.Text = "Write";
+            
+        }
+
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(textBox2.Text + textBox3.Text);
+            sw.Write(this.textBox1.Text);
+            MessageBox.Show("file has been written");
+            sw.Close();
+        }
+    }
+}
